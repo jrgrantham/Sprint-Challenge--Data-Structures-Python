7ea931e01711b56790ebe86af650fe3ea928f8d4
James Grantham <james.r.grantham@gmail.com>
2020-02-28 10:13:09 +0000

    reverse the list

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -42,6 +42,41 @@ class LinkedList:
     # if we've gotten here, then the target node isn't in our list
     return False
 
+  # def reverse_list(self):
+
+  #   # store elements to preserve original 'next' once its link is reversed
+  #   # start at the head and store current element
+  #   current_element = self.head
+  #   # initial previous element for the head doesn't exist
+  #   prev_element = None
+  #   # store the following element before changing the link
+  #   next_element = current_element.get_next()
+
+  #   while current_element:
+  #     current_element.set_next(prev_element)
+  #     current_element = next_element
+  #     prev = current_element
+  #   self.head = prev
+
+
+    # a -> b -> c -> d
+    
   def reverse_list(self):
-    # TO BE COMPLETED
-    pass
+
+      # store the initial element
+      current = self.head
+      # store the previous element
+      previous = None
+
+      # iterate until through list and swap next to previous
+      while current:
+          # store the 'next' element to preserve it.
+          next = current.next_node
+          # swap the current elements 'next' to 'previous'
+          current.next_node = previous
+          # update the 'previous' to current for next iteration
+          previous = current
+          # update the 'current' to the 'next' for next iteration
+          current = next
+      # once complete, set the head.
+      self.head = previous
